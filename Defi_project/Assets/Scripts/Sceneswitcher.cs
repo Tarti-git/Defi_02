1d98016b780664eb6f72f3b28296ba988c3e171f
TartifletteVEVO <56391305+TartifletteVEVO@users.noreply.github.com>
2020-02-12 14:02:26 +0100

    init

---
+++ Defi_project/Assets/Scripts/Sceneswitcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sceneswitcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void changescene(string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
